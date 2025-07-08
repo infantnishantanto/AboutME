aad44327fe8b29654f1862d974ae5d6450cb3faf
Infant <infantnishantanto@gmail.com>
2025-07-08 04:09:14 -0400

    Save images to MongoDB using GridFS

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const { check, validationResult } = require('express-validator')
-var session = require('express-session');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator');
+const session = require('express-session');
 const fileupload = require('express-fileupload');
 
-var app = express();
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +21,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Mongoose Models
 const User = mongoose.model('user', {
     userName: String,
     pass: String
@@ -28,75 +29,53 @@ const User = mongoose.model('user', {
 
 const Content = mongoose.model('content', {
     slug: String,
-    imPath: String,
+    image: {
+        data: Buffer,
+        contentType: String
+    },
     content: String
 });
 
-// mongoose.connect("mongodb://localhost:27017/Infant");
+// MongoDB Connection
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 .then(() => console.log("✅ MongoDB Atlas connected"))
 .catch(err => console.error("❌ Connection error:", err));
 
+// Routes
 app.get('/', (req, res) => {
-    Content.find({}).then((data) => {
+    Content.find({}).then(() => {
         res.redirect('/page/1');
-
-        
-
-    }).catch((err) => {
-        console.log(err);
-
-    });
+    }).catch(err => console.log(err));
 });
 
-
-
-app.get('/login', (req, res) => {
-    res.render('login')
-});
+app.get('/login', (req, res) => res.render('login'));
 
 app.post("/login", [
-    check('uname', "Usename Can not be empty").notEmpty(),
-    check('passwd', "Password Can not be empty").notEmpty(),
+    check('uname', "Username cannot be empty").notEmpty(),
+    check('passwd', "Password cannot be empty").notEmpty()
 ], (req, res) => {
-    console.log("Login Attempt:", req.body.uname, req.body.passwd);
-    var errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.render('login', { formErrors: errors.array() });
-    } else {
-        User.findOne({ userName: req.body.uname }).then((data) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.render('login', { formErrors: errors.array() });
 
-            if (data != null) {
-                if (data.pass == req.body.passwd) {
-
-                    req.session.logUser = data.userName;
-                    req.session.logStatus = true;
-                    res.redirect('/welcome/login');
-                } else {
-                    var authErrors = [{ "msg": "Password Wrong" }];
-                    res.render('login', { authErrors: authErrors });
-                }
-            } else {
-                var authErrors = [{ "msg": "UserName or Password Wrong" }];
-                res.render('login', { authErrors: authErrors });
-            }
-
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-
+    User.findOne({ userName: req.body.uname }).then(user => {
+        if (user && user.pass === req.body.passwd) {
+            req.session.logUser = user.userName;
+            req.session.logStatus = true;
+            res.redirect('/welcome/login');
+        } else {
+            res.render('login', { authErrors: [{ msg: "Username or Password incorrect" }] });
+        }
+    }).catch(err => console.log(err));
 });
 
-app.get('/welcome/:qobj', (req, res) => {
+app.get('/welcome/:msg', (req, res) => {
     if (req.session.logStatus) {
-        res.render('welcome', { logName: req.session.logUser, loggedIn: true, message: req.params.qobj });
-    }
-    else {
+        res.render('welcome', { logName: req.session.logUser, loggedIn: true, message: req.params.msg });
+    } else {
         res.redirect('/');
     }
 });
@@ -105,82 +84,57 @@ app.get('/add', (req, res) => {
     if (req.session.logStatus) {
         res.render('add', { logName: req.session.logUser });
     } else {
-        res.redirect('welcome');
+        res.redirect('/welcome/login');
     }
 });
 
-
 app.post('/add', [
-    check('slug', "Slug Can not be empty").notEmpty(),
-    check('tar', "Content Can not be empty").notEmpty()
+    check('slug', "Slug cannot be empty").notEmpty(),
+    check('tar', "Content cannot be empty").notEmpty()
 ], (req, res) => {
+    if (!req.session.logStatus) return res.redirect('/welcome/login');
 
-    if (req.session.logStatus) {
-        var errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.render('add', { logName: req.session.logUser, errors: errors.array() });
-        }
-
-        var pathStore;
-
-        if (req.files) {
-            var imName = req.files.imFile.name;
-            var im = req.files.imFile;
-            var path = 'public/uploads/' + imName;
-
-            im.mv(path, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            })
-            pathStore = '/uploads/' + imName;
-        }
-
-
-
-        var newContent = new Content({
-            slug: req.body.slug,
-            imPath: pathStore,
-            content: req.body.tar
-        });
-
-        newContent.save().then((data) => {
-            res.redirect('/welcome/Added Successfully');
-        }).catch((err) => {
-            console.log(err);
-        })
-
-
-    } else {
-        res.redirect('welcome');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render('add', { logName: req.session.logUser, errors: errors.array() });
     }
 
+    let newContent;
+    if (req.files && req.files.imFile) {
+        const im = req.files.imFile;
+        newContent = new Content({
+            slug: req.body.slug,
+            image: {
+                data: im.data,
+                contentType: im.mimetype
+            },
+            content: req.body.tar
+        });
+    } else {
+        newContent = new Content({
+            slug: req.body.slug,
+            content: req.body.tar
+        });
+    }
+
+    newContent.save().then(() => {
+        res.redirect('/welcome/Added Successfully');
+    }).catch(err => console.log(err));
 });
 
 app.get('/edit', (req, res) => {
-    if (req.session.logStatus) {
-        Content.find({}).then((data) => {
-            res.render('edit', { logName: req.session.logUser, data: data });
-        }).catch((err) => {
-            console.log(err)
-        })
-    } else {
-        res.redirect('/welcome/login');
-    }
-})
+    if (!req.session.logStatus) return res.redirect('/welcome/login');
 
-app.get('/delete/:ids&:type', (req, res) => {
+    Content.find({}).then(data => {
+        res.render('edit', { logName: req.session.logUser, data });
+    }).catch(err => console.log(err));
+});
 
-
-    Content.findOneAndDelete({ _id: req.params.ids }).then((data) => {
-        if (data != null) {
-            res.redirect('/edit')
-        } else {
-            Console.log("Could  not find record");
-        }
-
-    })
-})
+app.get('/delete/:id', (req, res) => {
+    Content.findByIdAndDelete(req.params.id).then(() => {
+        res.redirect('/edit');
+    }).catch(err => console.log(err));
+});
 
 app.get('/logout', (req, res) => {
     req.session.destroy();
@@ -188,99 +142,65 @@ app.get('/logout', (req, res) => {
 });
 
 app.get('/update/:id', (req, res) => {
+    if (!req.session.logStatus) return res.redirect('/welcome/login');
 
-    if (req.session.logStatus) {
-
-        Content.findOne({ _id: req.params.id }).then((data) => {
-
-            res.render('update', { data: data, loggedIn: true, logName: req.session.logUser });
-
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-
+    Content.findById(req.params.id).then(data => {
+        res.render('update', { data, loggedIn: true, logName: req.session.logUser });
+    }).catch(err => console.log(err));
 });
 
 app.post('/update', [
-    check('slug', "Slug Can not be empty").notEmpty(),
-    check('tar', "Content Can not be empty").notEmpty()
+    check('slug', "Slug cannot be empty").notEmpty(),
+    check('tar', "Content cannot be empty").notEmpty()
 ], (req, res) => {
-    console.log("Updating")
-    if (req.session.logStatus) {
-        var errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            Content.findOne({ _id: req.body.cMongoId }).then((data) => {
-                res.render('update', { data: data, errors: errors.array(), loggedIn: true, logName: req.session.logUser })
+    if (!req.session.logStatus) return res.redirect('/welcome/login');
 
-            }).catch((err) => {
-                console.log(err);
-            })
-        } else {
-            var storePath = req.body.cOldPath;
-            if (req.files) {
-                var imName = req.files.imFile.name;
-                var im = req.files.imFile;
-                var path = 'public/uploads/' + imName;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        Content.findById(req.body.cMongoId).then(data => {
+            res.render('update', { data, errors: errors.array(), loggedIn: true, logName: req.session.logUser });
+        });
+    } else {
+        Content.findById(req.body.cMongoId).then(data => {
+            data.slug = req.body.slug;
+            data.content = req.body.tar;
 
-                if (req.body.cOldPath != 'uploads/' + imName) {
-                    storePath = '/uploads/' + imName;
-                    im.mv(path).then(() => {
-
-                        console.log("New Image Upload Successful");
-                    }).catch((err) => {
-                        console.log(err);
-                    })
-                }
-
+            if (req.files && req.files.imFile) {
+                const im = req.files.imFile;
+                data.image = {
+                    data: im.data,
+                    contentType: im.mimetype
+                };
             }
-            Content.findOne({ _id: req.body.cMongoId }).then((data) => {
-                console.log(data);
 
-                data.slug = req.body.slug;
-                data.imPath = storePath;
-                data.content = req.body.tar;
-
-                data.save().then((data) => {
-                    
-                    res.redirect('/welcome/Edited Successfully',)
-                }).catch((err) => {
-                    console.log(err);
-                })
-            })
-
-        }
+            data.save().then(() => {
+                res.redirect('/welcome/Edited Successfully');
+            }).catch(err => console.log(err));
+        });
     }
-
-})
-
+});
 
 app.get('/page/:id', (req, res) => {
+    Content.find({}).then(data => {
+        let page = data.find(p => p._id.toString() === req.params.id) || data[0];
+        res.render('home', { logName: req.session.logUser, data, page });
+    }).catch(err => console.log(err));
+});
 
-   
-
-    Content.find({}).then((data) => {
-        var page = data[0];
-        for (item of data) {
-            if (item._id == req.params.id) {
-                page = item
-            }
+// Serve image from MongoDB
+app.get('/image/:id', (req, res) => {
+    Content.findById(req.params.id).then(data => {
+        if (data && data.image && data.image.data) {
+            res.contentType(data.image.contentType);
+            res.send(data.image.data);
+        } else {
+            res.status(404).send('Image not found');
         }
-
-        res.render('home', { logName: req.session.logUser, data: data, page: page });
-
-    }).catch((err) => {
-        console.log(err);
-
+    }).catch(err => {
+        console.error(err);
+        res.status(500).send('Error loading image');
     });
+});
 
-})
-
-
-
-app.listen(8080);
-console.log("Server Running...");
-
-
-
-
+// Start server
+app.listen(8080, () => console.log("✅ Server Running on http://localhost:8080"));
